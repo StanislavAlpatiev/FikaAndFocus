d68f063ff95dec87d9d8120b8a06fa4eaf444f9a
arvinrivers <90714951+arvinrivers@users.noreply.github.com>
2022-04-09 21:01:12 +0200

    First commit

---
+++ src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
@@ -0,0 +1,13 @@
+package com.dsv.pvt.fikafocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FikafocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FikafocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsv/pvt/fikafocus/FikafocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsv.pvt.fikafocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FikafocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
