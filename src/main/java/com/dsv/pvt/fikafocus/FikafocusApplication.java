2dbf7153907e5e69895822dfcdc17bfed941c21d
sabina <sabinanyqvist@hotmail.com>
2022-04-11 15:54:18 +0200

    test commit

--- src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
+++ src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
@@ -3,8 +3,6 @@ package com.dsv.pvt.fikafocus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// test sabina
-
 @SpringBootApplication
 public class FikafocusApplication {
 
