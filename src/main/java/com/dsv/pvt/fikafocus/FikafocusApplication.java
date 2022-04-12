28277e068a10fa583deb7726edc9d3c1f86c9b5f
Anton <anli6575@student.su.se>
2022-04-12 16:26:38 +0200

    test

--- src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
+++ src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class FikafocusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FikafocusApplication.class, args);
     }
 
+
+    @GetMapping
+    public String hello(){
+        return "hi";
+    }
 }
