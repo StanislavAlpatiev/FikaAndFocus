1aadc3014b34519bfe23e7a66960d9a72ab617e4
Anton <anli6575@student.su.se>
2022-04-12 16:20:23 +0200

    test

--- src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
+++ src/main/java/com/dsv/pvt/fikafocus/FikafocusApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FikafocusApplication {
 
+    // test 220412 15:42:4111
     public static void main(String[] args) {
         SpringApplication.run(FikafocusApplication.class, args);
     }
