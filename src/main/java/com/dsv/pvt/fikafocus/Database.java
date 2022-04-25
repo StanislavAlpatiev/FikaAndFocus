24f14a9a5107f6feabdde1047227444f30b1345c
Axel Davidsson <axel.davidsson@hotmail.com>
2022-04-25 19:08:17 +0200

    la till mariadb och sql i maven dependencies. skapa Database-class för test av läsa från databas

---
+++ src/main/java/com/dsv/pvt/fikafocus/Database.java
@@ -0,0 +1,31 @@
+package com.dsv.pvt.fikafocus;
+
+import java.sql.*;
+
+public class Database {
+
+
+    public static void main(String[] args) {
+        try {
+
+            //connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mariadb://mysql.dsv.su.se:3306/test_data", "axda2670", "Ua5rah6eengu");
+
+            //create statement
+            Statement myStmt = myConn.createStatement();
+
+            //exectue SQL-query on reviews(table) in test_data(database)
+            ResultSet myRs = myStmt.executeQuery("select * from reviews");
+
+            //show result
+            while (myRs.next()){
+                System.out.println(myRs.getString("name") + "   "  + myRs.getString("content"));
+            }
+
+        } catch (SQLException throwables) {
+                throwables.printStackTrace();
+        }
+    }
+ }
+
+
