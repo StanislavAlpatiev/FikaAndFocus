1c5477c5e4d1e872a2d3206abb38409be4031478
sabina <sabinanyqvist@hotmail.com>
2022-05-13 11:21:56 +0200

    updated Bookmarks.dart

--- fika_and_fokus/lib/Bookmarks.dart
+++ fika_and_fokus/lib/Bookmarks.dart
@@ -3,16 +3,11 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'Cafe.dart';
 
-class BookmarksPage extends StatefulWidget {
-  const BookmarksPage({Key? key}) : super(key: key);
-
-  @override
-  State<BookmarksPage> createState() => _BookmarksPageState();
-}
-
 class CafeItem {
   final String name;
   final Widget place;
+  //final String price;
+  //final String rating;
 
   CafeItem(this.name, this.place);
 
@@ -23,37 +18,73 @@ class CafeItem {
   Widget returnIcon(BuildContext context) {
     return place;
   }
+
+  factory CafeItem.fromJson(Map<String, dynamic> json) {
+    Widget tempPlace = Icon(Icons.coffee);
+
+    return CafeItem(json['name'], tempPlace);
+    //return CafeItem(json['username'], tempPlace);
+  }
+}
+
+class BookmarksPage extends StatefulWidget {
+  const BookmarksPage({Key? key}) : super(key: key);
+
+  @override
+  State<BookmarksPage> createState() => _BookmarksPageState();
 }
 
 class _BookmarksPageState extends State<BookmarksPage> {
+  var cafes = [];
 
-  final cafes = List<CafeItem>.generate(5,
-          (i) => CafeItem('Cafe $i', const Icon(Icons.coffee)));
+  @override
+  void initState() {
+    refreshCafes();
+    super.initState();
+  }
+
+  Future refreshCafes() async {
+    Uri favoriteCafesURI = Uri.parse('https://group-1-75.pvt.dsv.su.se/cafes/all');
+    //Uri favoriteCafesURI = Uri.parse('https://chiseled-spotted-individual.glitch.me/rand');
+
+    final response = await http.get(favoriteCafesURI);
+    var data = json.decode(response.body);
+
+    cafes = [];
+    var _cafesTemp = [];
+    for (var i = 0; i < data.length; i++) {
+      _cafesTemp.add(CafeItem.fromJson(data[i]));
+    }
+    setState(() {
+      cafes = _cafesTemp;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[100], //BACKGROUND COLOR
+        backgroundColor: Colors.blue[100],
         appBar: AppBar(
           title: const Text('Favorites'),
         ),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: cafes.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: cafes[index].place,
-                  title: cafes[index].buildTitle(context),
-                );
-              },
+            child: RefreshIndicator(
+              onRefresh: refreshCafes,
+              child: ListView.builder(
+                itemCount: cafes.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: cafes[index].place,
+                    title: cafes[index].buildTitle(context),
+                  );
+                },
+              ),
             ),
           )
         ],
       )
     );
-
-    // body: const Center(child: Text('Name: ', style: TextStyle(fontSize: 20))),
   }
 }
