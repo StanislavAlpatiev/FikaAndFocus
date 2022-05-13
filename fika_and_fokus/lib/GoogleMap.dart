cbad39282de253645603d48268c7c4d1d9038369
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2022-05-13 21:26:23 +0200

    implemented currentLocation as part of the bestTime query

--- fika_and_fokus/lib/GoogleMap.dart
+++ fika_and_fokus/lib/GoogleMap.dart
@@ -35,14 +35,14 @@ class _MyGoogleMapState extends State<MyGoogleMap> {
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    _handlePermission();
   }
 
   //callback from FilterWindow Widget/Class
   callback(businessLevel, radius, callbackStatus) {
     setState(() {
       if(callbackStatus == "search") {
-        _requestLocationPermission(businessLevel, radius);
+        _createMarkers(businessLevel.round().toString(), radius.round().toString());
       }
       _changeVisibility();
       //test för att se i konsollen
@@ -139,27 +139,25 @@ class _MyGoogleMapState extends State<MyGoogleMap> {
     });
   }
 
-  _getCurrentLocation() {
-    Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-      });
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handlePermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    final position = await GeolocatorPlatform.instance.getCurrentPosition();
+    setState(() {
+      _currentPosition = position;
     });
   }
 
   _animateToUser() async {
-    Position location = await GeolocatorPlatform.instance.getCurrentPosition();
-    // _currentPosition = location;
-    // mapController
-    //     .animateCamera(CameraUpdate.newCameraPosition(const CameraPosition(
-      // target: LatLng(location.latitude, location.longitude),
-      // target: LatLng(59.3247819, 18.0699477),
+    _getCurrentPosition();
     mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-      target: LatLng(location.latitude, location.longitude),
+      target: LatLng(_currentPosition.latitude, _currentPosition.longitude),
       zoom: 17.0,
     )));
-    // print(_currentPosition.toString());
   }
 
   _addMarker() async {
@@ -188,7 +186,7 @@ class _MyGoogleMapState extends State<MyGoogleMap> {
                 businessLevel +
                 "&radius=" +
                 radius +
-                "&lng=18.0699477&lat=59.3247819");
+                "&lng=" + _currentPosition.longitude.toString() + "&lat=" + _currentPosition.latitude.toString());
     // Uri.parse("http://192.168.1.80:8080/cafes/locations?busy_min=0&busy_max=100&radius=500&lng=18.0699477&lat=59.3247819");
     // Uri.parse("http://192.168.0.14:8080/cafes/locations?busy_min=0&busy_max=100&radius=500&lng=18.0699477&lat=59.3247819");
     // {"status":"OK","venues":[{"day_info":{"day_int":3,"day_max":49,"day_mean":29,"day_rank_max":4,"day_rank_mean":4,"day_text":"Thursday","venue_closed":17,"venue_open":8},"day_int":3,"day_raw":[0,0,10,15,20,20,25,35,45,50,35,0,0,0,0,0,0,0,0,0,0,0,0,0],"day_raw_whole":[0,0,10,15,20,20,25,35,45,50,35,0,0,0,0,0,0,0,0,0,0,0,0,0],"price_level":2,"rating":4.2,"reviews":958,"venue_address":"Stora Nygatan 32 111 27 Stockholm Sweden","venue_dwell_time_max":60,"venue_dwell_time_min":30,"venue_id":"ven_496233356c534f6569305552593058334a2d56455958624a496843","venue_lat":59.3238939,"venue_lng":18.0696015,"venue_name":"Caf\u00e9 Kladdkakan","venue_type":"CAFE"},{"day_info":{"day_int":3,"day_max":20,"day_mean":9,"day_rank_max":3,"day_rank_mean":4,"day_text":"Thursday","venue_closed":20,"venue_open":10},"day_int":3,"day_raw":[0,0,0,0,0,5,10,15,20,20,10,5,5,5,0,0,0,0,0,0,0,0,0,0],"day_raw_whole":[0,0,0,0,0,5,10,15,20,20,10,5,5,5,0,0,0,0,0,0,0,0,0,0],"price_level":0,"rating":4.4,"reviews":172,"venue_address":"Skomakargatan 9 111 29 Stockholm Sweden","venue_dwell_time_max":60,"venue_dwell_time_min":30,"venue_id":"ven_7758763247775f495a433152593058334a4f394b6535674a496843","venue_lat":59.3244017,"venue_lng":18.0709397,"venue_name":"Coffee Stop","venue_type":"CAFE"},{"day_info":{"day_int":3,"day_max":49,"day_mean":33,"day_rank_max":6,"day_rank_mean":5,"day_text":"Thursday","venue_closed":20,"venue_open":7},"day_int":3,"day_raw":[0,10,20,30,40,50,50,45,45,50,45,30,15,5,0,0,0,0,0,0,0,0,0,0],"day_raw_whole":[0,10,20,30,40,50,50,45,45,50,45,30,15,5,0,0,0,0,0,0,0,0,0,0],"price_level":2,"rating":4.2,"reviews":763,"venue_address":"V\u00e4sterl\u00e5nggatan 57 111 29 Stockholm Sweden","venue_dwell_time_max":60,"venue_dwell_time_min":30,"venue_id":"ven_30755f585479317a7a5f4f52593058334a2d39373877764a496843","venue_lat":59.3240556,"venue_lng":18.0707138,"venue_name":"Espresso House","venue_type":"CAFE"},{"day_info":{"day_int":3,"day_max":37,"day_mean":22,"day_rank_max":5,"day_rank_mean":4,"day_text":"Thursday","venue_closed":21,"venue_open":9},"day_int":3,"day_raw":[0,0,0,5,5,10,25,35,35,30,20,25,30,30,15,0,0,0,0,0,0,0,0,0],"day_raw_whole":[0,0,0,5,5,10,25,35,35,30,20,25,30,30,15,0,0,0,0,0,0,0,0,0],"price_level":2,"rating":3.9,"reviews":576,"venue_address":"Stora Nygatan 37 111 27 Stockholm Sweden","venue_dwell_time_max":0,"venue_dwell_time_min":0,"venue_id":"ven_347145374a71636876366952593058334a652d374d52744a496843","venue_lat":59.3235766,"venue_lng":18.0703647,"venue_name":"Caf\u00e9 Kronan","venue_type":"CAFE"},{"day_info":{"day_int":3,"day_max":58,"day_mean":33,"day_rank_max":3,"day_rank_mean":4,"day_text":"Thursday","venue_closed":22,"venue_open":9},"day_int":3,"day_raw":[0,0,0,15,25,30,35,40,45,55,60,55,40,25,10,5,0,0,0,0,0,0,0,0],"day_raw_whole":[0,0,0,15,25,30,35,40,45,55,60,55,40,25,10,5,0,0,0,0,0,0,0,0],"price_level":2,"rating":4.2,"reviews":724,"venue_address":"Stortorget 20 111 29 Stockholm Sweden","venue_dwell_time_max":0,"venue_dwell_time_min":0,"venue_id":"ven_4953584f30694d6431344552593058334a2d593058346c4a496843","venue_lat":59.3249056,"venue_lng":18.0703528,"venue_name":"Kaffekoppen","venue_type":"CAFE"}],"venues_n":5,"window":{"day_window":"Thursday 6AM until Friday 5AM","day_window_end_int":4,"day_window_end_txt":"Friday","day_window_start_int":3,"day_window_start_txt":"Thursday","time_local":18,"time_local_12":"6PM","time_local_index":12,"time_window_end":5,"time_window_end_12h":"5AM","time_window_end_ix":23,"time_window_start":6,"time_window_start_12h":"6AM","time_window_start_ix":0}}
@@ -260,23 +258,45 @@ class _MyGoogleMapState extends State<MyGoogleMap> {
     });
   }
 
-  _requestLocationPermission(dynamic businessLevel, dynamic radius) async {
-    final serviceStatusLocation = await Permission.locationWhenInUse.status;
-    final serviceStatusLocationAlways = await Permission.locationAlways.status;
+  Future<bool> _handlePermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-    // bool isLocation = serviceStatusLocation == ServiceStatus.enabled;
+    // Test if location services are enabled.
+    serviceEnabled = await GeolocatorPlatform.instance.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      GeolocatorPlatform.instance.requestPermission();
 
-    final status = await Permission.locationWhenInUse.request();
-
-    if (!serviceStatusLocationAlways.isGranted) {
-      await Permission.locationAlways.request();
+      return false;
     }
-    if (serviceStatusLocationAlways.isGranted) {
-      _createMarkers(
-          businessLevel.round().toString(), radius.round().toString());
-    } else if (serviceStatusLocationAlways.isPermanentlyDenied) {
-      // showToast("Camera needs to access your microphone, please provide permission", position: ToastPosition.bottom);
-      await openAppSettings();
+
+    permission = await GeolocatorPlatform.instance.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await GeolocatorPlatform.instance.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        GeolocatorPlatform.instance.requestPermission();
+
+        return false;
+      }
     }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      GeolocatorPlatform.instance.openLocationSettings();
+      // GeolocatorPlatform.instance.requestPermission();
+      return false;
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return true;
   }
 }
