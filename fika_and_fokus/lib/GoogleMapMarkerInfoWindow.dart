a73edd925ff0a2ec52ff9973bf4a04f79c627e3b
MarekMazur <marekmazur.piano@gmail.com>
2022-05-23 10:37:02 +0200

    GoogleMapMarkerInfoWindow.dart design changed

--- fika_and_fokus/lib/GoogleMapMarkerInfoWindow.dart
+++ fika_and_fokus/lib/GoogleMapMarkerInfoWindow.dart
@@ -4,12 +4,13 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'CafeItemModel.dart';
 import 'CafePage.dart';
-import 'package:cupertino_icons/cupertino_icons.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:map_launcher/map_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MarkerInfoWindow extends StatefulWidget {
   String markedVenueName;
@@ -42,9 +43,9 @@ class _MarkerInfoWindowState extends State<MarkerInfoWindow> {
     TextDecoration.overline,
     TextDecoration.underline
   ];
-  Icon notFilledHeart = Icon(CupertinoIcons.heart, color: Colors.red);
-  Icon filledHeart = Icon(CupertinoIcons.heart_fill, color: Colors.red);
-  late Icon heart;
+  FaIcon notFilledHeart = FaIcon(FontAwesomeIcons.heart, color: Colors.red);
+  FaIcon filledHeart = FaIcon(FontAwesomeIcons.solidHeart, color: Colors.red);
+  late FaIcon heart;
   late bool isHeartFilled;
 
   void goToGoogleMapsApp() {
@@ -56,191 +57,191 @@ class _MarkerInfoWindowState extends State<MarkerInfoWindow> {
     return Visibility(
       child: GestureDetector(
         child: Container(
-
-            // color: Colors.red,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: ExactAssetImage('images/coffieShop_.jpg'),
-                    fit: BoxFit.fitWidth,
-                    colorFilter: ColorFilter.linearToSrgbGamma()),
-                // border: Border.all(
-                //     color: Colors.red
-                // ),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  stops: const [
+                    0.05,
+                    0.4,
+                    0.7,
+                  ],
+                  colors: const [
+                    Color(0xFFE0DBCF),
+                    Color(0xFF75AB98),
+                    Color(0xFF696969),
+                  ],
+                ),
                 borderRadius: BorderRadius.only(
                     topRight: Radius.circular(20),
                     topLeft: Radius.circular(20)),
-                //borderRadius: BorderRadius.all(Radius.circular(20)),
                 boxShadow: [
                   BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
+                      color: Colors.grey.withOpacity(0.9),
                       spreadRadius: 5,
                       blurRadius: 7,
                       offset: Offset(2, 3))
                 ]),
             child: Container(
-              // color: Color(0xFF75AB98),
               margin: EdgeInsets.only(
-                  left: 60.0, top: 0.0, right: 60.0, bottom: 0.0),
+                  left: 40.0, top: 0.0, right: 40.0, bottom: 0.0),
               height: 170,
               // width: double.infinity,
               child: Column(
                 children: [
-                  SizedBox(
-                    // could be sizebox instead of row
-                    height: 5.0,
-                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Padding(
-                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        padding: EdgeInsets.only(top: 10, bottom: 5),
                         child: Text(widget.markedVenueName,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
-                                fontFamily: 'Roboto',
-                                //backgroundColor: Colors.white,
-                                fontSize: 25,
-                                decoration: TextDecoration.combine(dec),
-                                decorationColor: Colors.white,
-                                decorationStyle: TextDecorationStyle.double,
-                                decorationThickness: 2),
+                            style: GoogleFonts.oswald(
+                              textStyle: const TextStyle(letterSpacing: .5),
+                              fontWeight: FontWeight.w400,
+                              fontSize: 25,
+                              color: Color(0xFFFFFFFF)),
                             textAlign: TextAlign.left),
                       ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Card(
-                        child: Row(children: [
-                          Text(
-                            widget.markedRating.toString(),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                           Row(children: [
+                            Text(
+                              widget.markedRating.toString(),
+                              textAlign: TextAlign.left,
+                              style: GoogleFonts.roboto(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              child: Icon(
+                                Icons.star,
+                                color: Colors.white,
+                                // color: Colors.amber,
+                                size: 25,
+                              ),
+                            ),
+                          ]),
+                        Text(" " + widget.markedDistance,
                             textAlign: TextAlign.left,
                             style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w900),
-                          ),
-                          Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                            size: 25,
-                          ),
-                        ]),
-                      ),
-                      Text(" " + widget.markedDistance,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 25,
-                          ))
-                    ],
+                              fontSize: 25,
+                            ))
+                      ],
+                    ),
                   ),
-                  SizedBox(
-                    height: 0.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: Card(
-                          child: IconButton(
-                            iconSize: 40,
-                            icon: Icon(
-                              Icons.info_outline,
-                              color: Colors.black,
-                            ),
-                            onPressed: () {
-                              // CafeItem cafeItem = CafeItem(widget.markedVenueId, widget.markedVenueName, widget.markedPriceLevel.toString(), widget.markedRating);
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => CafePage(CafeItem(
-                                          widget.markedVenueId,
-                                          widget.markedVenueName,
-                                          widget.markedAddress,
-                                          widget.markedLat,
-                                          widget.markedLong,
-                                          widget.markedPriceLevel.toString(),
-                                          widget.markedRating))));
-                            },
-                          ),
-                        ),
-                      ),
-                      Card(
-                        shape:
-                            ShapeBorder.lerp(CircleBorder(), CircleBorder(), 2),
-                        child: FutureBuilder(
-                            future: _checkIfCafeIsFavorite(),
-                            builder: (context, snapshot) {
-                              if (snapshot.data == true) {
-                                return IconButton(
-                                    onPressed: () {
-                                      _toggleHeart();
-                                    },
-                                    iconSize: 47,
-                                    icon: filledHeart);
-                              } else {
-                                return IconButton(
-                                    onPressed: () {
-                                      _toggleHeart();
-                                    },
-                                    iconSize: 50,
-                                    icon: notFilledHeart);
-                              }
-                            }),
-                      ),
-                      Container(
-                        // height: 50,
-                        // width: 100,
-                        child: Card(
-                          child: IconButton(
-                            onPressed: () async {
-                              // if (await MapLauncher.isMapAvailable(MapType.google)) {
-                              // await MapLauncher.showMarker(
-                              //   mapType: MapType.google,
-                              //   coords: Coords(widget.markedLat, widget.markedLong),
-                              //   title: "going to " + widget.markedVenueName,
-                              //   // description: description,
-                              // );
-                              if (MapLauncher != null){
-                                if (await MapLauncher.isMapAvailable(MapType.googleGo) as bool) {
-                                  await MapLauncher.showDirections(
-                                    mapType: MapType.googleGo,
-                                    destination: Coords(widget.markedLat, widget.markedLong),
-                                    //origin: Coords(59.2971115,17.7642534);
-                                    destinationTitle: "going to " + widget.markedVenueName,
-                                    // description: description,
-                                  );
-                                }
-                                else if (await MapLauncher.isMapAvailable(MapType.google) as bool) {
-                                  await MapLauncher.showDirections(
-                                    mapType: MapType.google,
-                                    destination: Coords(widget.markedLat, widget.markedLong),
-                                    //origin: Coords(59.2971115,17.7642534);
-                                    destinationTitle: "going to " + widget.markedVenueName,
-                                    // description: description,
-                                  );
-                                }
-                              }
-
-
-                              // }
-
-                            },
-                            iconSize: 40,
-                            icon: Icon(
-                              Icons.directions_walk,
-                              color: Colors.black,
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Color(0x22FFFFFF),
+                        borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: IconButton(
+                              iconSize: 56,
+                              icon: Icon(
+                                CupertinoIcons.info_circle,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                // CafeItem cafeItem = CafeItem(widget.markedVenueId, widget.markedVenueName, widget.markedPriceLevel.toString(), widget.markedRating);
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => CafePage(CafeItem(
+                                            widget.markedVenueId,
+                                            widget.markedVenueName,
+                                            widget.markedAddress,
+                                            widget.markedLat,
+                                            widget.markedLong,
+                                            widget.markedPriceLevel.toString(),
+                                            widget.markedRating))));
+                              },
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        Card(
+                          shape:
+                              ShapeBorder.lerp(CircleBorder(), CircleBorder(), 2),
+                          child: FutureBuilder(
+                              future: _checkIfCafeIsFavorite(),
+                              builder: (context, snapshot) {
+                                if (snapshot.data == true) {
+                                  return IconButton(
+                                      onPressed: () {
+                                        _toggleHeart();
+                                      },
+                                      iconSize: 35,
+                                      icon: filledHeart);
+                                } else {
+                                  return IconButton(
+                                      onPressed: () {
+                                        _toggleHeart();
+                                      },
+                                      iconSize: 35,
+                                      icon: notFilledHeart);
+                                }
+                              }),
+                        ),
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                            child: IconButton(
+                              onPressed: () async {
+                                // if (await MapLauncher.isMapAvailable(MapType.google)) {
+                                // await MapLauncher.showMarker(
+                                //   mapType: MapType.google,
+                                //   coords: Coords(widget.markedLat, widget.markedLong),
+                                //   title: "going to " + widget.markedVenueName,
+                                //   // description: description,
+                                // );
+                                if (MapLauncher != null){
+                                  if (await MapLauncher.isMapAvailable(MapType.googleGo) as bool) {
+                                    await MapLauncher.showDirections(
+                                      mapType: MapType.googleGo,
+                                      destination: Coords(widget.markedLat, widget.markedLong),
+                                      //origin: Coords(59.2971115,17.7642534);
+                                      destinationTitle: "going to " + widget.markedVenueName,
+                                      // description: description,
+                                    );
+                                  }
+                                  else if (await MapLauncher.isMapAvailable(MapType.google) as bool) {
+                                    await MapLauncher.showDirections(
+                                      mapType: MapType.google,
+                                      destination: Coords(widget.markedLat, widget.markedLong),
+                                      //origin: Coords(59.2971115,17.7642534);
+                                      destinationTitle: "going to " + widget.markedVenueName,
+                                      // description: description,
+                                    );
+                                  }
+                                }
+                              },
+                              iconSize: 44,
+                              icon: FaIcon(
+                                FontAwesomeIcons.personWalking,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            )),
+            )
+        ),
       ),
     );
   }
