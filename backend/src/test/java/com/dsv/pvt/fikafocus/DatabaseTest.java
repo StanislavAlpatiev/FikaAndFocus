5f287bce27e0ff6c61b4df9f4e701bcddb28c1c1
Your Name <arsol93@hotmail.com>
2022-05-24 13:10:22 +0200

    more unit tests

--- backend/src/test/java/com/dsv/pvt/fikafocus/DatabaseTest.java
+++ backend/src/test/java/com/dsv/pvt/fikafocus/DatabaseTest.java
@@ -1,5 +1,5 @@
 /**
- * application.properties in /src/main/resources needs to be changed to direct to test_data before running tests
+ * application.properties in /src/main/resources needs to be changed to direct to test_data databse before running tests
  * You will also need to run spring boot locally and use local urls instead of tomcat
  */
 
@@ -10,6 +10,8 @@ import com.dsv.pvt.fikafocus.cafe.Cafe2;
 import com.dsv.pvt.fikafocus.cafe.CafeRepository;
 import com.dsv.pvt.fikafocus.review.ReviewRepository;
 import com.dsv.pvt.fikafocus.user.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +33,9 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 @ActiveProfiles("test")
@@ -54,8 +59,6 @@ public class DatabaseTest {
             stmt.executeQuery("delete from review");
             stmt.executeQuery("delete from cafe2");
             stmt.executeQuery("delete from user");
-            String date = LocalDate.now().toString();
-            System.out.println(date);
         } catch (
                 SQLException throwables) {
             throwables.printStackTrace();
@@ -216,6 +219,124 @@ public class DatabaseTest {
                     " " + myRs.getString("cafe") + " " + myRs.getString("user");;
         }
         assertEquals("1 " + date + " 4 excellent coffee abc123 abc123@gmail.com", s);
+    }
+
+    @Test
+    void favouriteByUserIdReturnsCorrectJSON() throws IOException, SQLException{
+        String sqlInsert = "insert into cafe2 (id, address, name, lat, lng, price, rating) " +
+                "values ('abc123', 'café street 123', 'café abc123', '123.456', '1.01101', '4', '3')";
+        stmt.executeUpdate(sqlInsert);
+        String sqlInsert2 = "insert into user (email, username, pass) values ('abc123@gmail.com', 'abc123', '123abc')";
+        stmt.executeUpdate(sqlInsert2);
+        String sqlInsert3 = "insert into favourites (cafe_id, user_id) " +
+                "values ('abc123', 'abc123@gmail.com')";
+        stmt.executeUpdate(sqlInsert3);
+        String data = "";
+        URL url = new URL("http://127.0.0.1:8080/cafes/abc123@gmail.com/favourites");
+        URLConnection connection = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) connection;
+        http.setRequestMethod("GET");
+        http.setDoOutput(true);
+        StringBuilder informationString = new StringBuilder();
+        Scanner scanner = new Scanner(url.openStream());
+
+        while (scanner.hasNext()) {
+            informationString.append(scanner.nextLine());
+        }
+        scanner.close();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(String.valueOf(informationString));
+        String cafe = jsonNode.at("/0/id").toString();
+        assertEquals("\"abc123\"", cafe);
+    }
+
+    @Test
+    void favouriteByCafeIdReturnsCorrectJSON() throws IOException, SQLException{
+        String sqlInsert = "insert into cafe2 (id, address, name, lat, lng, price, rating) " +
+                "values ('abc123', 'café street 123', 'café abc123', '123.456', '1.01101', '4', '3')";
+        stmt.executeUpdate(sqlInsert);
+        String sqlInsert2 = "insert into user (email, username, pass) values ('abc123@gmail.com', 'abc123', '123abc')";
+        stmt.executeUpdate(sqlInsert2);
+        String sqlInsert3 = "insert into favourites (cafe_id, user_id) " +
+                "values ('abc123', 'abc123@gmail.com')";
+        stmt.executeUpdate(sqlInsert3);
+        String data = "";
+        URL url = new URL("http://127.0.0.1:8080/cafes/abc123/favouritesbycafe");
+        URLConnection connection = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) connection;
+        http.setRequestMethod("GET");
+        http.setDoOutput(true);
+        StringBuilder informationString = new StringBuilder();
+        Scanner scanner = new Scanner(url.openStream());
+
+        while (scanner.hasNext()) {
+            informationString.append(scanner.nextLine());
+        }
+        scanner.close();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(String.valueOf(informationString));
+        String cafe = jsonNode.at("/0/email").toString();
+        assertEquals("\"abc123@gmail.com\"", cafe);
+    }
+
+    @Test
+    void removingFavouriteByHTTP() throws IOException, SQLException{
+        String sqlInsert = "insert into cafe2 (id, address, name, lat, lng, price, rating) " +
+                "values ('abc123', 'café street 123', 'café abc123', '123.456', '1.01101', '4', '3')";
+        stmt.executeUpdate(sqlInsert);
+        String sqlInsert4 = "insert into cafe2 (id, address, name, lat, lng, price, rating) " +
+                "values ('abc1234', 'café street 123', 'café abc1234', '123.456', '1.01101', '4', '3')";
+        stmt.executeUpdate(sqlInsert4);
+        String sqlInsert2 = "insert into user (email, username, pass) values ('abc123@gmail.com', 'abc123', '123abc')";
+        stmt.executeUpdate(sqlInsert2);
+        String sqlInsert3 = "insert into favourites (cafe_id, user_id) " +
+                "values ('abc123', 'abc123@gmail.com')";
+        stmt.executeUpdate(sqlInsert3);
+        String sqlInsert5 = "insert into favourites (cafe_id, user_id) " +
+                "values ('abc1234', 'abc123@gmail.com')";
+        stmt.executeUpdate(sqlInsert5);
+        String data = "";
+        URL url = new URL("http://127.0.0.1:8080/cafes/abc123@gmail.com/removefavourite/abc1234");
+        URLConnection connection = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) connection;
+        http.setRequestMethod("DELETE");
+        http.setDoOutput(true);
+        connection.getOutputStream().write(data.getBytes(StandardCharsets.UTF_8));
+        connection.getInputStream();
+        ResultSet myRs = stmt.executeQuery("select * from favourites");
+        String s = null;
+        if ( myRs.next() ) {
+            s = myRs.getString("cafe_id") + " " + myRs.getString("user_id");
+        }
+        assertEquals("abc123 abc123@gmail.com", s);
+    }
+
+    @Test
+    void reviewByUserIdReturnsCorrectJSON() throws IOException, SQLException{
+        String sqlInsert = "insert into cafe2 (id, address, name, lat, lng, price, rating) " +
+                "values ('abc123', 'café street 123', 'café abc123', '123.456', '1.01101', '4', '3')";
+        stmt.executeUpdate(sqlInsert);
+        String sqlInsert2 = "insert into user (email, username, pass) values ('abc123@gmail.com', 'abc123', '123abc')";
+        stmt.executeUpdate(sqlInsert2);
+        String sqlInsert3 = "insert into review (id, date, rating, review_string, cafe, user) " +
+                "values (1, '2020-12-22', 4, 'excellent coffee', 'abc123', 'abc123@gmail.com')";
+        stmt.executeUpdate(sqlInsert3);
+        String data = "";
+        URL url = new URL("http://127.0.0.1:8080/reviews/users/abc123@gmail.com");
+        URLConnection connection = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) connection;
+        http.setRequestMethod("GET");
+        http.setDoOutput(true);
+        StringBuilder informationString = new StringBuilder();
+        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            informationString.append(scanner.nextLine());
+        }
+        scanner.close();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(String.valueOf(informationString));
+        String cafe = jsonNode.at("/0/review_string").toString();
+        assertEquals("\"excellent coffee\"", cafe);
     }
 
 
