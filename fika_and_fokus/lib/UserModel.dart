554b62ec6292800b169bbb3d20259b0654f88c19
DarkNight <farhadgunners@gmail.com>
2022-05-24 12:46:44 +0200

    added first junit test for usermodel frontend

--- fika_and_fokus/lib/UserModel.dart
+++ fika_and_fokus/lib/UserModel.dart
@@ -9,13 +9,11 @@ class UserModel {
   String _userName = "";
   String _password = "";
 
-  UserModel(
-    {
-      required userName,
-      required email,
-      required password,
-    }
-  );
+  UserModel({
+    required userName,
+    required email,
+    required password,
+  });
 
   UserModel.login(String username, String pass) {
     username = username;
@@ -23,26 +21,24 @@ class UserModel {
   }
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
-      email: json["email"],
-      userName: json["username"],
-      password: json["pass"]);
+      email: json["email"], userName: json["username"], password: json["pass"]);
 
-  Map<String, dynamic> toJson() => {
-        "email": _email,
-        "username": _userName,
-        "pass": _password
-      };
+  Map<String, dynamic> toJson() =>
+      {"email": _email, "username": _userName, "pass": _password};
 
-  String get getEmail => _email.trim(); // if user accidentally added space at the end
+  String get getEmail =>
+      _email.trim(); // if user accidentally added space at the end
   String get getUserName => _userName.trim();
   String get getPassword => _password;
 
   set password(String value) {
     _password = value;
   }
+
   set userName(String value) {
     _userName = value;
   }
+
   set email(String value) {
     _email = value;
   }
