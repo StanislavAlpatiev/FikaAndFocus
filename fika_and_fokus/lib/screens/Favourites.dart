922b53d75e597630d39f80ac7bc1fb1e34980180
MarekMazur <marekmazur.piano@gmail.com>
2022-05-30 10:59:02 +0200

    Refactoring Favourites

--- fika_and_fokus/lib/screens/Favourites.dart
+++ fika_and_fokus/lib/screens/Favourites.dart
@@ -8,7 +8,7 @@ import 'CafePage.dart';
 import '../models/CafeIModel.dart';
 
 class BookmarksPage extends StatefulWidget {
-  UserModel user = new UserModel(userName: "", email: "", password: "");
+  UserModel user = UserModel(userName: "", email: "", password: "");
 
   BookmarksPage(UserModel user, {Key? key}) : super(key: key) {
     this.user = user;
@@ -42,53 +42,49 @@ class _BookmarksPageState extends State<BookmarksPage> {
         automaticallyImplyLeading: false,
       ),
       body: SafeArea(
-        child: Container(
-          child: RefreshIndicator(
-            onRefresh: refreshCafes,
-            child: ListView.builder(
-              itemCount: cafes.length,
-              itemBuilder: (context, index) => Dismissible(
-                background: Container(
-                  color: Colors.red,
-                ),
-                key: ValueKey<CafeModel>(cafes[index]),
-                onDismissed: (DismissDirection direction) {
-                  setState(() {
-                    CafeModel current = cafes[index];
-                    cafes.removeAt(index);
-                    _deleteCafeFromFavorites(current.id);
-                  });
-                },
-                child: Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.all(5),
-                  child: ListTile(
-                    leading: const Icon(
-                      Icons.coffee,
-                      size: 56.0,
-                      color: Color(0xFFB95815),
-                    ),
-                    title: cafes[index].buildTitle(context),
-                    subtitle: cafes[index].buildPrice(context),
-                    trailing: RatingBarIndicator(
-                      rating: cafes[index].buildRating(context),
-                      direction: Axis.horizontal,
-                      itemCount: 5,
-                      itemSize: 10.0,
-                      itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
-                      itemBuilder: (context, _) => const Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  CafePage(cafes[index], widget.user)));
-                    },
+        child: RefreshIndicator(
+          onRefresh: refreshCafes,
+          child: ListView.builder(
+            itemCount: cafes.length,
+            itemBuilder: (context, index) => Dismissible(
+              background: Container(
+                color: Colors.red,
+              ),
+              key: ValueKey<CafeModel>(cafes[index]),
+              onDismissed: (DismissDirection direction) {
+                setState(() {
+                  CafeModel current = cafes[index];
+                  cafes.removeAt(index);
+                  _deleteCafeFromFavorites(current.id);
+                });
+              },
+              child: Card(
+                elevation: 5,
+                margin: const EdgeInsets.all(5),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.coffee,
+                    size: 56.0,
+                    color: Color(0xFFB95815),
                   ),
+                  title: cafes[index].buildTitle(context),
+                  subtitle: cafes[index].buildPrice(context),
+                  trailing: RatingBarIndicator(
+                    rating: cafes[index].buildRating(context),
+                    direction: Axis.horizontal,
+                    itemCount: 5,
+                    itemSize: 10.0,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+                    itemBuilder: (context, _) => const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  onTap: () { Navigator.push(
+                    context, MaterialPageRoute(
+                      builder: (context) =>
+                      CafePage(cafes[index], widget.user)));
+                  },
                 ),
               ),
             ),
@@ -100,17 +96,13 @@ class _BookmarksPageState extends State<BookmarksPage> {
 
   Future refreshCafes() async {
     Uri favoriteCafesURI = Uri.parse(
-        'https://group-1-75.pvt.dsv.su.se/fikafocus-0.0.1-SNAPSHOT/cafes/' +
-            widget.user.getEmail +
-            '/favourites');
-    // Uri favoriteCafesURI = Uri.parse('https://group-1-75.pvt.dsv.su.se/fikafocus-0.0.1-SNAPSHOT/cafes/sten@gmail.com/favourites');
-
+      'https://group-1-75.pvt.dsv.su.se/fikafocus-0.0.1-SNAPSHOT/cafes/' +
+      widget.user.getEmail + '/favourites');
     
     final response = await http.get(favoriteCafesURI);
 
     if (response.statusCode == 200) {
-      String source = Utf8Decoder().convert(response.bodyBytes);
-      //var data = json.decode(response.body);
+      String source = const Utf8Decoder().convert(response.bodyBytes);
       var data = json.decode(source);
 
       cafes = [];
@@ -120,7 +112,6 @@ class _BookmarksPageState extends State<BookmarksPage> {
       }
 
       _cafesTemp.sort((a, b) => a.name.compareTo(b.name));
-      print(_cafesTemp.toString());
       setState(() {
         cafes = _cafesTemp;
       });
@@ -131,9 +122,9 @@ class _BookmarksPageState extends State<BookmarksPage> {
 
   _deleteCafeFromFavorites(String mvid) async {
     Uri deleteFavouriteUrl =
-        Uri.parse('https://group-1-75.pvt.dsv.su.se/fikafocus-0.0.1-SNAPSHOT/'
-            'cafes/${widget.user.getEmail}/removefavourite/${mvid}');
+      Uri.parse('https://group-1-75.pvt.dsv.su.se/fikafocus-0.0.1-SNAPSHOT/'
+          'cafes/${widget.user.getEmail}/removefavourite/$mvid');
 
-    final response = await http.delete(deleteFavouriteUrl);
+    await http.delete(deleteFavouriteUrl);
   }
 }
