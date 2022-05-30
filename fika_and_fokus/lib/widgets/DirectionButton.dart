1c7c6443e66e7c4a7d0a7cf0325ff5386da9e898
sabina <sabinanyqvist@hotmail.com>
2022-05-30 11:01:09 +0200

    refactoring DirectionButton.dart

--- fika_and_fokus/lib/widgets/DirectionButton.dart
+++ fika_and_fokus/lib/widgets/DirectionButton.dart
@@ -1,24 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:map_launcher/map_launcher.dart';
-
 import '../models/CafeIModel.dart';
 
 class DirectionButton extends StatefulWidget {
-  CafeModel currentCafe;
+  final CafeModel currentCafe;
 
-  DirectionButton({Key? key, required this.currentCafe}) : super(key: key) {
-    currentCafe = currentCafe;
-  }
-
+  const DirectionButton({Key? key, required this.currentCafe})
+      : super(key: key);
 
   @override
   State<DirectionButton> createState() => _DirectionButtonState();
 }
 
 class _DirectionButtonState extends State<DirectionButton> {
-
   @override
   void initState() {
     super.initState();
@@ -28,49 +23,29 @@ class _DirectionButtonState extends State<DirectionButton> {
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () async {
-        // if (await MapLauncher.isMapAvailable(MapType.google)) {
-        // await MapLauncher.showMarker(
-        //   mapType: MapType.google,
-        //   coords: Coords(widget.markedLat, widget.markedLong),
-        //   title: "going to " + widget.markedVenueName,
-        //   // description: description,
-        // );
         if (MapLauncher != null) {
-          if (await MapLauncher.isMapAvailable(
-              MapType.googleGo) as bool) {
+          if (await MapLauncher.isMapAvailable(MapType.googleGo) as bool) {
             await MapLauncher.showDirections(
               mapType: MapType.googleGo,
-              destination: Coords(
-                  widget.currentCafe.lat,
-                  widget.currentCafe.long),
-              //origin: Coords(59.2971115,17.7642534);
-              destinationTitle:
-              "going to " + widget.currentCafe.name,
-              // description: description,
+              destination:
+                  Coords(widget.currentCafe.lat, widget.currentCafe.long),
+              destinationTitle: "going to " + widget.currentCafe.name,
             );
-          } else if (await MapLauncher.isMapAvailable(
-              MapType.google) as bool) {
+          } else if (await MapLauncher.isMapAvailable(MapType.google) as bool) {
             await MapLauncher.showDirections(
               mapType: MapType.google,
-              destination: Coords(
-                  widget.currentCafe.lat,
-                  widget.currentCafe.long),
-              //origin: Coords(59.2971115,17.7642534);
-              destinationTitle:
-              "going to " + widget.currentCafe.name,
-              // description: description,
+              destination:
+                  Coords(widget.currentCafe.lat, widget.currentCafe.long),
+              destinationTitle: "going to " + widget.currentCafe.name,
             );
           }
         }
       },
       iconSize: 44,
-      icon: FaIcon(
+      icon: const FaIcon(
         FontAwesomeIcons.personWalkingArrowRight,
         color: Colors.white,
       ),
     );
   }
 }
-
-
-
